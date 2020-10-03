d97a61a7e6c927e3681d17fd849fd45c2cc78477
Daniel Oleynyk <den00058@hotmail.com>
2020-10-04 00:21:53 +0300

    updates for template

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,41 +1,46 @@
-import { Link } from 'gatsby'
-import React from 'react'
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
 
 interface Props {
   siteTitle: string
 }
 
-export default class Header extends React.Component<Props> {
-  render() {
-    const { siteTitle } = this.props
-
-    return (
-      <div
-        style={{
-          background: 'rebeccapurple',
-          marginBottom: '1.45rem',
-        }}
-      >
-        <div
+const Header = ({ siteTitle }: Props) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
           style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '1.45rem 1.0875rem',
+            color: `white`,
+            textDecoration: `none`,
           }}
         >
-          <h1 style={{ margin: 0 }}>
-            <Link
-              to="/"
-              style={{
-                color: 'white',
-                textDecoration: 'none',
-              }}
-            >
-              {siteTitle}
-            </Link>
-          </h1>
-        </div>
-      </div>
-    )
-  }
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
 }
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,10 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
-import NameForm from '../components/name_form'
 
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
-    <NameForm />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>

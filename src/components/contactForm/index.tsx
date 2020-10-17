cd929adb57dd780cd93bb499681725933c4caf3c
Daniel Oleynyk <den00058@hotmail.com>
2020-10-17 17:09:34 +0300

    fixed contact form

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -1,33 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { navigate } from 'gatsby'
 import styles from './contact_form.module.css'
 
 const ContactForm = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const encode = (data: { [key: string]: any }) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&')
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const form = e.currentTarget
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.name,
+        name: name,
+        email: email,
+        message: message,
+      }),
+    })
+      .then(() => navigate(form.action))
+      .catch((error) => alert(error))
+  }
+
   return (
     <div className={styles.root}>
       <h1 style={{ fontSize: '1.5em', width: '80%' }}>
-        Lorem ipsum Lorem ipsoum ipsum Lorem ipsoum ipsum Lorem ipsoum ipsum
-        Lorem ipsoum ipsum Lorem ipsoumLorem ipsoum ipsum Lorem ipsoum ipsum
-        Lorem ipsoumLorem ipsoum ipsum Lorem ipsoum ipsum Lorem ipsoumLorem
-        ipsoum ipsum Lorem ipsoum ipsum Lorem ipsoum
+        Do you want to work with me?
       </h1>
       <div>
-        <form name="contact" method="POST" data-netlify="true">
+        <form
+          name="contact"
+          method="post"
+          action="/contact/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
           <p>
             <label>
               Name: <br />
-              <input type="text" name="name" />
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </label>
           </p>
           <p>
             <label>
               Email: <br />
-              <input type="email" name="email" />
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </label>
           </p>
           <p>
             <label>
               Message: <br />
-              <textarea name="message" rows={5}></textarea>
+              <textarea
+                name="message"
+                rows={5}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
             </label>
           </p>
           <p>
